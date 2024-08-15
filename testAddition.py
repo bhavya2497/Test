bcd44da69932a5a0058ad036315010a0c5112a69
bhavya2497 <akhila.bhavya@gmail.com>
2024-08-15 15:06:41 +1000

    new function

--- testAddition.py
+++ testAddition.py
@@ -7,4 +7,6 @@ addition_function(var1,var2)
 def sub_function(var1, var2):
     print("difference value: ", (var2-var1))
 sub_function(var1, var2)
-
+def multiplication_function(var1, var2):
+    print("multiplication value:", (var2*var1))
+multiplication_function(var1, var2)
