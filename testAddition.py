06b31df410f50fd6f4d472cac5c729e2dd3fa2d1
bhavya2497 <akhila.bhavya@gmail.com>
2024-08-15 12:48:35 +1000

    added sub_function function to the file

--- testAddition.py
+++ testAddition.py
@@ -1,7 +1,9 @@
+var1 = 4
+var2 = 9
 def addition_function(var1, var2):
     print("sum value:", (var1+var2))
 
-var1 = 4
-var2 = 9
 addition_function(var1,var2)
+def sub_function(var1, var2):
+    print("difference value: ", (var2-var1))
 
