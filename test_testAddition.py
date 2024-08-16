3a7bba2f081a9b4358d8d88c57cd6a50aaa3a614
bhavya2497 <akhila.bhavya@gmail.com>
2024-08-16 14:55:01 +1000

    changes

--- test_testAddition.py
+++ test_testAddition.py
@@ -6,6 +6,6 @@ class TestCalculations(unittest.TestCase):
     def test_sum(self):
         calculation = Calculations(8, 2)
         self.assertEqual(calculation.get_sum(), 10, 'The sum is wrong.')
-        self.assertEqual(calculation.get_sum(), 15, 'The sum is wrong.')
+
 if __name__ == '__main__':
     unittest.main()
