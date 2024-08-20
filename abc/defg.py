e4917fb46172d00b3f48e12f325a6c56eaca3fff
bhavya2497 <akhila.bhavya@gmail.com>
2024-08-20 17:24:20 +1000

    new changes

--- abc/defg.py
+++ abc/defg.py
@@ -2,5 +2,5 @@ class MultiplicationAndDivision:
     def get_product(self):
             return self.a * self.b
 
-        def get_quotient(self):
+    def get_quotient(self):
             return self.a / self.b
